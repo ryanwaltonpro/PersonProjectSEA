a90bd236ac4f03edc05c75131d80ba8a5c9045ce
Ryan Walton <ryanwalton1999@gmail.com>
2022-09-26 11:43:35 +0100

    created person repo

--- src/main/java/com/example/personproject/repositories.java
+++
@@ -1,4 +0,0 @@
-package com.example.personproject;
-
-public class repositories {
-}
---
+++ src/main/java/com/example/personproject/repositories/PersonRepo.java
@@ -0,0 +1,14 @@
+package com.example.personproject.repositories;
+
+import com.example.personproject.entities.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PersonRepo extends JpaRepository<Person, Long> {
+
+    List<Person> fundPersonByName(String name);
+
+}
